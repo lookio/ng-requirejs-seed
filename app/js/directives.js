ab83bb26c0e71b588ae989d51c7b6f52e94795c2
Drew Baumann <drewbaumann@gmail.com>
2014-06-25 19:26:40 -0700

    using directives for button handling.

--- app/js/directives.js
+++ app/js/directives.js
@@ -12,23 +12,25 @@ define(['angular', 'services', 'jquery'], function(angular, services, $) {
           templateUrl: 'app/partials/chat-button.html',
           link: function(s, e, a){
 
-            // var  $chat    =  $(e)
-            //   ,  $parent  =  $chat.parent()
-            //   ,  account  =  $parent.data('account')
-            //   ,  skill    =  $parent.data('skill');
+            var  $chat    =  $(e)
+              ,  $parent  =  $chat.parent()
+              ,  account  =  String($parent.data('account'))
+              ,  skill    =  String($parent.data('skill'));
 
-            // console.log(account);
-            // console.log(skill);
+            $chat.attr('id', account+"-"+skill+"-chatButton");
+            console.log(account);
+            console.log(skill);
 
-            // if ( !LPMobile.getEnabled(account, skill)  ){
-            //   console.log('disabled');
-            //   $chat.hide()
-            // }else{
-            //   console.log('enabled');
-            //   $chat.on('click', function(){
-            //     LPMobile.beginChat(account, skill);
-            //   })
-            // }
+            if ( !LPMobile.getEnabled(account, skill)  ){
+              console.log('disabled');
+              $chat.hide()
+            }else{
+              console.log('enabled');
+              $chat.on('click', function(){
+                LPMobile.beginChat(account, skill);
+                return false;
+              })
+            }
           }
         }
   }])
@@ -40,23 +42,27 @@ define(['angular', 'services', 'jquery'], function(angular, services, $) {
           templateUrl: 'app/partials/dsl-care.html',
           link: function(s, e, a){
 
-            // var  $chat    =  $(e)
-            //   ,  $parent  =  $chat.parent()
-            //   ,  account  =  $parent.data('account')
-            //   ,  skill    =  $parent.data('skill');
+            // Service for button enabling goes here.
+            var  $chat    =  $(e)
+              ,  $parent  =  $chat.parent()
+              ,  account  =  String($parent.data('account'))
+              ,  skill    =  String($parent.data('skill'));
 
-            // console.log(account);
-            // console.log(skill);
+            $chat.attr('id', account+"-"+skill+"-chatButton");
+            console.log(account);
+            console.log(skill);
 
-            // if ( !LPMobile.getEnabled(account, skill)  ){
-            //   console.log('disabled');
-            //   $chat.hide()
-            // }else{
-            //   console.log('enabled');
-            //   $chat.on('click', function(){
-            //     LPMobile.beginChat(account, skill);
-            //   })
-            // }
+            if ( !LPMobile.getEnabled(account, skill)  ){
+              console.log('disabled');
+              $chat.hide()
+            }else{
+              console.log('enabled');
+              $chat.on('click', function(){
+                LPMobile.beginChat(account, skill);
+                return false;
+              })
+            }
+
           }
         }
   }]);
